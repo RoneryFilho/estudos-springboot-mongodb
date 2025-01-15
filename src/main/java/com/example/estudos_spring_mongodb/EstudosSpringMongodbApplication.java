ed948b187aab5902f0592c4d68dc4f9033ddb4ed
Ronery Filho <roneryrffilho@gmail.com>
2025-01-15 20:50:02 -0300

    Primeiro commit

---
+++ src/main/java/com/example/estudos_spring_mongodb/EstudosSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudos_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estudos_spring_mongodb/EstudosSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudos_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
