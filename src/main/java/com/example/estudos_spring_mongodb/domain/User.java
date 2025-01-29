3ad7dc68e52f7c25b89fa8bb0f841a33b15cae43
Ronery Filho <roneryrffilho@gmail.com>
2025-01-29 18:19:42 -0300

    Referenciando os posts do user

--- src/main/java/com/example/estudos_spring_mongodb/domain/User.java
+++ src/main/java/com/example/estudos_spring_mongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.example.estudos_spring_mongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true) // lazy = true é para a lista de posts só ser chamada quando feito de forma explicita
+    private List<Post> posts = new ArrayList<>();
 
     public User(){
 
@@ -47,6 +53,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
